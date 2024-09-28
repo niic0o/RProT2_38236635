b739274c9d0bca01c2124cba12d742a543b78717
Nico :) <nicosena10@gmail.com>
2024-09-28 15:28:48 -0300

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/RProT2_38236635';
+    public string $baseURL = 'http://localhost/RProT2_nico';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
